9128807b534e50e8a48e8301f73ae55058b2c7ca
Shiyan Boxer <shiyanboxer7@gmail.com>
2021-01-13 00:16:31 -0500

    update nav

---
+++ src/components/Project.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import Emoji from "./Emoji";
+
+class Project extends Component {
+    render() {
+        return (
+            <div className="section">
+                <h3>
+                    Feel free to reach out, I love to chat! <Emoji symbol="☕"/>
+                </h3>
+                <h3>
+
+
+                </h3>
+                <a
+                    href="https://github.com/shiyanboxer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Built with <Emoji symbol="❤"/> by Shiyan Boxer
+                </a>
+
+
+            </div>
+        );
+    }
+}
+
+export default Project;
